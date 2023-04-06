98f441b9e3f6fd6311275b3d1166a84ef08e5134
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-04-06 16:20:48 +0200

    rdy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [count, setCount] = useState(0);
+  let [value, setValue] = useState(0);
+
+  const einzahlen = () => {
+    setCount(count + value)
+  }
+
+  const auszahlen = () => {
+    setCount(count - value)
+  }
+
+  let change = (elt) => {
+    setValue(Number(elt.target.value))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header className='header'>
+          <h1 id='title'>Banana Bank</h1>
+        </header>
+        <main>
+          <section className='konto'>
+            <div className='saldo'> {count} €</div>
+            <input type="number" name="" onChange={change} value={value} id="geldbetrag" placeholder='Gib einen Geldbetrag ein' /><br />
+            <button className='einzahlen' onClick={einzahlen}>Einzahlen</button>
+            <button className='auszahlen' onClick={auszahlen}>Auszahlen</button>
+          </section>
+        </main>
+      </body>
     </div>
   );
 }
